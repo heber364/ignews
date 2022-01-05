abc6c9213d92087c2c0fb1d294f7c718f150c652
Héber Lima Silva <heberlimasilva@gmail.com>
2022-01-05 09:52:06 -0300

    Modulo ' Back-end no front-end' finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,37 @@
 import { SignInButton } from '../SignInButton'
-
+import { VscMenu , VscClose} from 'react-icons/vsc'
 import styles from './styles.module.scss'
+import { useState } from 'react'
 
 export function Header() {
+
+	const [idNavMenu, setIdNavMenu] = useState('nav-menu')
+
+    function handleOpenToggle(){
+        setIdNavMenu('show-menu');
+    }
+
+    function handleCloseToggle(){
+        setIdNavMenu('nav-menu');
+    }
+
 	return (
 		<header className={styles.headerContainer}>
 			<div className={styles.headerContent}>
 				<img src="/images/logo.svg" alt="ig.news" />
-				<nav>
-					<a className={styles.active} href="#">Home</a>
-					<a href="#">Posts</a>
-				</nav>
-				<SignInButton />
+					<div className={styles.menuContent} id={idNavMenu}>
+						<nav>
+							<a className={styles.active} href="#" onClick={handleCloseToggle}>Home</a>
+							<a href="#" onClick={handleCloseToggle}>Posts</a>
+							<SignInButton />
+						</nav>
+						
+					</div>
+				{ idNavMenu === 'nav-menu' 
+				? <VscMenu color="#fff" size="30" onClick={handleOpenToggle}/> 
+				: <VscClose color="#fff" size="30" onClick={handleCloseToggle}/>
+					
+				}
 			</div>
 		</header>
 	)
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +9,34 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId } : SubscribeButtonProps){
+    const [ session ] = useSession();
+
+    async function handleSubscribe(){
+        if(!session){
+            signIn('github');
+            return;
+        }
+
+        try {
+            const resoponse = await api.post('./subscribe')
+            
+            const { sessionId } = resoponse.data;
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId})
+        } catch(err){
+            alert(err.message);
+        }
+            
+       
+    }
+   
+   
     return(
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
